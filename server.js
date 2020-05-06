708f6c2231e2079ac60e3bf44b72f7001042cda7
MROD <mriguez777@gmail.com>
2020-05-06 00:11:18 -0600

    Created files and Started on the folder structure. Installed npm packets: express, express-cli, handlebars, nodemon, mysql and mysql2. Created the code on server.js to connect to mysql and tested connection successfully

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mysql = require("mysql");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var connection = mysql.createConnection({
+  host: "127.0.0.1",
+  port: 3306,
+  user: "root",
+  password: "working",
+  database: "burgers_db",
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
