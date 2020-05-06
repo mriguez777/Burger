84911d7d471837de07b3fbbfddf7e6af6c528f10
MROD <mriguez777@gmail.com>
2020-05-06 01:36:07 -0600

    Created and updated the .sql files. schema and seeds are the two files, schema has the code to create the db and table, seeds has the code to insert in the starter burgers for this assignment.

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,14 @@
+
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
-CREATE TABLE burgers
-(
-    id INT NOT NULL
-    AUTO_INCREMENT,
-burger_name VARCHAR
-    (100),
-devoured BOOLEAN,
-PRIMARY KEY
-    (id)
-)
+CREATE TABLE burgers(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(100),
+    devoured BOOLEAN,
+    PRIMARY KEY(id)
+);
+
+SELECT * FROM Burgers;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("PeanutButter Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Western Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Its Not Beef Burger", TRUE);
