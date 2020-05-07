ba05fb9f91e3eda8e5d40386abdf2a39882631d2
MROD <mriguez777@gmail.com>
2020-05-06 18:20:08 -0600

    fixing heroku link

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,18 @@
 //Starting mysql connection on this page
 var mysql = require("mysql");
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    //Connection details
+    var connection = mysql.createConnection({
+        host: "127.0.0.1",
+        port: 3306,
+        user: "root",
+        password: "working",
+        database: "burgers_db"
+    });
+}
 
-var PORT = process.env.PORT || 8080;
-
-//Connection details
-var connection = mysql.createConnection({
-    host: "127.0.0.1",
-    port: 3306,
-    user: "root",
-    password: "working",
-    database: "burgers_db"
-});
 
 //connecting action 
 connection.connect(function (err) {
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require("express");
 
-var PORT = process.env.PORT || 8080;
-
 var app = express();
-
+var PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
 
